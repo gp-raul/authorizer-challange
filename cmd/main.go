482f6d66fd9af514adc3b7698a3d2b961a5c1475
Raul Guilherme <gp.raul@outlook.com>
2020-07-03 07:06:34 -0300

    adicionando o repository

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/gpraul/authorizer-challange/cmd/adapter"
+	"github.com/gpraul/authorizer-challange/cmd/core"
 	"github.com/gpraul/authorizer-challange/cmd/parser"
 )
 
 func main() {
 	jsonAccount := `{"account": {"active-card": true, "available-limit": 100}}`
 
-	parser.Parse(jsonAccount)
+	// parser do json
+	operation, err := parser.Parse(jsonAccount)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// salvando a conta em um slice
+	account := operation.(core.Account)
+	adapter.SaveAccount(account)
+
+	account, err = adapter.FindOneAccount()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Nova conta:", account)
+
 }
--- cmd/parser/parser.go
+++ cmd/parser/parser.go
@@ -3,48 +3,46 @@ package parser
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"regexp"
 
 	"github.com/gpraul/authorizer-challange/cmd/core"
 )
 
-func Parse(s string) {
+// Parse function
+func Parse(s string) (interface{}, error) {
 	r := regexp.MustCompile(".+account.+")
 	if r.MatchString(s) {
 		account, _ := parseAccount(s)
-		fmt.Println(account)
-		return
+		return account, nil
 	}
 
 	r = regexp.MustCompile(".+transaction.+")
 	if r.MatchString(s) {
 		transaction, _ := parseTransaction(s)
-		fmt.Println(transaction)
-		return
+		return transaction, nil
 	}
 
-	fmt.Println("Formato não encontrado...")
+	return nil, errors.New("Formato não encontrado")
 }
 
-func parseAccount(s string) (*core.Account, error) {
+func parseAccount(s string) (core.Account, error) {
 	a := &core.Account{}
 
 	err := json.Unmarshal([]byte(s), a)
 	if err != nil {
-		return nil, errors.New("Erro ao converter para o tipo Account")
+		return core.Account{}, errors.New("Erro ao converter para o tipo Account")
 	}
 
-	return a, nil
+	return *a, nil
 }
 
-func parseTransaction(s string) (*core.Transaction, error) {
+func parseTransaction(s string) (core.Transaction, error) {
 	t := &core.Transaction{}
 
 	err := json.Unmarshal([]byte(s), t)
 	if err != nil {
-		return nil, errors.New("Erro ao converter para o tipo Transaction")
+		return core.Transaction{}, errors.New("Erro ao converter para o tipo Transaction")
 	}
 
-	return t, nil
+	return *t, nil
 }
